1e4af9e6f7ea06973e0df890056d98c93b7980dc
J. Victor Martins <jvdm@sdf.org>
2016-05-22 18:43:47 -0300

    Add guess commands

--- macacoprego/mastermind_bot/__init__.py
+++ macacoprego/mastermind_bot/__init__.py
@@ -1,3 +1,12 @@
+# mastermind-bot - A mastermind bot for slack
+# © 2015 João Victor Duarte Martins <jvdm@sdf.org>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+import os
 import json
 import re
 import logging
@@ -16,7 +25,8 @@ log = logging.getLogger(__name__)
 
 
 def get_api_url(api, **kwds):
-    return ('https://mastermind-macacoprego.herokuapp.com/{}/'
+    #return ('http://localhost:8000/api/{}/'
+    return ('https://mastermind-macacoprego.herokuapp.com/api/{}/'
             .format(api.format(**kwds)))
 
 
@@ -25,8 +35,6 @@ class MastermindBot(SlackBot):
     VERSION = __version__
 
     loop = None
-
-    players = {}
 
     # FIXME We need to overwrite this method because we want to access
     #       the socket later on.  We changed only the `self.socket`
@@ -66,7 +74,6 @@ class MastermindBot(SlackBot):
         self.socket.send_str(json.dumps(payload))
 
     def _reply_to(self, msg, text):
-        log.info(msg)
         return {'channel': msg['channel'],
                 'text': '<@{user}>: {text}'
                     .format(user=msg['user'], text=text)}
@@ -75,18 +82,23 @@ class MastermindBot(SlackBot):
         return (self.message_is_to_me(msg) and
                 msg['text'].lstrip(self.address_as).startswith(command))
 
-    def filter_create_command(self, msg):
-        return self._filter_command(msg, 'create')
-
     def _reply_callback(self, msg, fut):
         try:
             text = fut.result()
         except:
-            text = ('sorry man, something bad '
-                    'happend ({})'.format(fut.exception()))
+            text = ("sorry guys, couldn't create your game, something bad "
+                    "happend: {}".format(fut.exception()))
             log.exception('request failed')
         self.send_message(**self._reply_to(msg, text))
 
+    def filter_create_command(self, msg):
+        return self._filter_command(msg, 'create')
+
+    def filter_guess_command(self, msg):
+        if not self._filter_command(msg, 'guess'):
+            return False
+        return re.search(r'#(\d+) (\w+)', msg['text'])
+            
     async def dispatch_create_command(self, msg):
         players = re.findall(r'<@\w+>', msg['text'].lstrip(self.address_as))
         # The creator is just another player.
@@ -94,33 +106,78 @@ class MastermindBot(SlackBot):
         asyncio.ensure_future(self.create_game(msg, players),
                               loop=self.loop) \
                .add_done_callback(functools.partial(self._reply_callback, msg))
-        return self._reply_to(msg, 'nice, a new game is on the way...')
+        return self._reply_to(msg,
+                              'sure, give me a second to create '
+                              'a game for you...')
+
+    async def dispatch_guess_command(self, msg):
+        match = re.search(r'#(\d+) (\w+)', msg['text'])
+        game_id, code = match.groups()
+        with aiohttp.ClientSession() as session:
+            async with session.post(get_api_url('games/{id}/guess',
+                                                id=game_id),
+                                    data={'name': '<@{}>'.format(msg['user']),
+                                          'code': code}) \
+                    as resp:
+                data = await resp.json()
+                print(data)
+                if resp.status != 201:
+                    return self._reply_to(msg, 'bad guess')
+                return self._reply_to(
+                    msg,
+                    '{} exacts, {} nears (total guess: {})'
+                    .format(data['exact'], data['near'], data['num_guesses']))
 
     async def create_game(self, msg, players):
         log.info('creating game for %s', players)
-        api_url = get_api_url('games')
         with aiohttp.ClientSession() as session:
-            async with session.post(api_url, data={'players_count': len(players)}) \
+            async with session.post(get_api_url('games'),
+                                    data={'players_count': len(players)}) \
                     as resp:
+                text = await resp.text()
+                if resp.status != 201:
+                    raise Exception(text)
                 game_info = await resp.json()
-            return ("done guys, game #{id} was created with "
-                    "{players_count} players ({names}), you may "
-                    "make a guess to this game by sending messages like "
+            # Join every player listed in the create command.
+            for player in players:
+                async with session.post(get_api_url('games/{id}/join',
+                                               id=game_info['id']),
+                                        data={'name': player}) \
+                        as resp:
+                    text = await resp.text()
+                    if resp.status != 201:
+                        raise Exception(text)
+                    log.info('player %s has joined game %s: %s',
+                             player, game_info['id'], text)
+            return ("the game #{id} has been created, there are "
+                    "{players_count} players ({names}), you all may "
+                    "send guesses by sending messages like "
                     "'guess #{id} <your guess>'."
                     .format(names=', '.join(players), **game_info))
 
     MESSAGE_FILTERS = {
-        filter_create_command: dispatch_create_command}
+        filter_create_command: dispatch_create_command,
+        filter_guess_command: dispatch_guess_command}
+
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser(description='A mastermind bot for slack.')
+    parser.add_argument(
+        '--token',
+        help='slack bot token')
+    return parser.parse_args()
 
 
 def main():
-
+    args = parse_args()
     logging.basicConfig(
         datefmt='%H:%M:%S',
         format='%(asctime)s %(name)s [%(levelname)s] %(message)s',
         level=logging.INFO)
-
-    token = 'xoxb-44792020947-fGA0luNrhKyIpr6PyN6PksKN'
+    token = args.token
+    if not token:
+        token = os.environ.get('MACACOPREGO_MASTERMIND_BOT_TOKEN')
     loop = asyncio.get_event_loop()
     bot = loop.run_until_complete(MastermindBot.from_api_token(token))
     # FIXME Usually we would pass this to __init__, but this aslack
